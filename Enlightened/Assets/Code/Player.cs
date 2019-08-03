126b9d8729502b482aaddd41645be55212d27328
Paradoxical <paradoxical@echo-studios.co.uk>
2019-08-03 14:55:37 -0700

    Animations and basic movment

--- Enlightened/Assets/Code/Player.cs
+++ Enlightened/Assets/Code/Player.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
+    private Rigidbody2D rig;
     // Start is called before the first frame update
     void Start()
     {
-
+        rig = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
-        transform.Translate(movement);
+    void Update() {
+        Vector3 movement = new Vector2(Input.GetAxisRaw("Horizontal") * speed, 0);
+        rig.MovePosition(transform.position + (movement * Time.deltaTime));
+        if (/*Input.GetAxisRaw("Vertical") < 0 ||*/ Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            print("Player Is Crouching");
+        }
+        if (/*Input.GetAxisRaw("Vertical") > 0 ||*/ Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Player Is Jumping");
+            rig.AddForce(new Vector2(0,jumpForce));
+        }
     }
 }
