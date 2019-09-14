01aa1819c1d123dfb28d73c6a1f8c92519405074
BustosMan <bustosman@echo-studios.co.uk>
2019-09-14 00:47:08 -0700

    Added 2D lighting to the project, some levels are dark for now. Possible merge conflict if you don't have LWRP installed on your engine.
    Please make a backup of your project before pulling this change.

---
+++ Enlighten/Assets/Code/ButtonAnimHandler.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAnimHandler : MonoBehaviour
+{
+    public Text menuText;
+
+    Color initialTextColor = new Color();
+    Color glowColor = new Color(.2941f, .8549f, .7529f);
+
+    [HideInInspector]
+    public bool animDone;
+
+    Animator[] anims;
+
+    bool hovering;
+    bool canHover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anims = GetComponentsInChildren<Animator>();
+        canHover = true;
+        animDone = false;
+        initialTextColor = menuText.color;
+    }
+
+    private void Update()
+    {
+        if (hovering && canHover)
+        {
+            foreach (Animator anim in anims)
+            {
+                if (anim.transform.name == "Right")
+                {
+                    anim.SetBool("Right", true);
+                }
+                else if (anim.transform.name == "Left")
+                {
+                    anim.SetBool("Left", true);
+                }
+                else if (anim.transform.name == "Middle")
+                {
+                    anim.SetBool("Middle", true);
+                }
+            }
+        }
+
+        foreach (Animator anim in anims)
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Left Select") || anim.GetCurrentAnimatorStateInfo(0).IsName("Mid Select") || 
+                anim.GetCurrentAnimatorStateInfo(0).IsName("Right Select"))
+            {
+                menuText.color = glowColor;
+                if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f) { animDone = true; }
+            }
+        }
+
+        if (transform.name == "Exit Button Image" && animDone)
+        {
+            Application.Quit();
+        }
+        else if (transform.name == "Play Button Image" && animDone)
+        {
+            UIManager.instance.onSelect.Invoke();
+        }
+    }
+
+    public void OnMouseOver()
+    {
+        hovering = true;
+        menuText.color = glowColor;
+    }
+
+    public void OnMouseExit()
+    {
+        hovering = false;
+
+        menuText.color = initialTextColor;
+
+        foreach (Animator anim in anims)
+        {
+            if (anim.transform.name == "Right")
+            {
+                anim.SetBool("Right", false);
+            }
+            else if (anim.transform.name == "Left")
+            {
+                anim.SetBool("Left", false);
+            }
+            else if (anim.transform.name == "Middle")
+            {
+                anim.SetBool("Middle", false);
+            }
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        menuText.color = glowColor;
+
+        foreach (Animator anim in anims)
+        {
+            if (anim.transform.name == "Right")
+            {
+                anim.SetBool("Right", false);
+                anim.Play("Right Select");
+
+                if (anim.GetCurrentAnimatorStateInfo(0).loop)
+                {
+                    canHover = false;
+                }
+                else
+                {
+                    canHover = true;
+                }
+            }
+            else if (anim.transform.name == "Left")
+            {
+                anim.SetBool("Left", false);
+                anim.Play("Left Select");
+
+                if (anim.GetCurrentAnimatorStateInfo(0).loop)
+                {
+                    canHover = false;
+                }
+                else
+                {
+                    canHover = true;
+                }
+            }
+            else if (anim.transform.name == "Middle")
+            {
+                anim.SetBool("Middle", false);
+                anim.Play("Mid Select");
+
+                if (anim.GetCurrentAnimatorStateInfo(0).loop)
+                {
+                    canHover = false;
+                }
+                else
+                {
+                    canHover = true;
+                }
+            }
+        }
+    }
+}
---
+++ Enlighten/Assets/Code/UIManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class UIManager : MonoBehaviour
+{
+    [HideInInspector]
+    public UnityEvent onSelect = new UnityEvent();
+
+    ButtonAnimHandler btnHandler;
+
+    #region References
+    [SerializeField]
+    private GameObject menuCanvas, levelCanvas;
+    [SerializeField]
+    private Button playButton, playLevelButton, exitButton;
+    [SerializeField]
+    private int sceneCount;
+    #endregion
+
+    #region Singleton
+
+    public static UIManager instance = null;
+
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+        {
+            instance = this;
+        }
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+    }
+
+    #endregion
+
+    private void Start()
+    {
+        if(PlayerPrefs.HasKey("UnlockedLevel"))
+            SetupCanvases();
+
+        //playButton.onClick.AddListener(Play);
+        //exitButton.onClick.AddListener(Application.Quit);
+        onSelect.AddListener(Play); //for the image play button
+    }
+
+    private void SetupCanvases()
+    {
+        SetupLevelButtons();
+        levelCanvas.SetActive(false);
+    }
+
+    private void SetupLevelButtons()
+    {
+        int startingX = -500;
+        int startingY = 400;
+
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < sceneCount; i++)
+        {
+            int a = i; //to store level index helper
+            if (i % 5 == 0)
+            {
+                y++;
+                x = 0;
+            }
+
+            else
+                x++;
+
+            Button current = Instantiate(playLevelButton.gameObject, new Vector3(startingX + (x * 250), startingY + (y * -200), 0), Quaternion.identity).GetComponent<Button>();
+            current.transform.SetParent(levelCanvas.transform, false);
+            current.GetComponentInChildren<Text>().text = (i + 1).ToString();
+            current.onClick.AddListener(delegate { PlayLevel(a + 1); });
+
+            if (PlayerPrefs.GetInt("UnlockedLevel") < (a + 1))
+                current.interactable = false;
+        }
+    }
+
+    #region UI Functions
+    private void Play()
+    {
+        if (!PlayerPrefs.HasKey("UnlockedLevel"))
+            SceneManager.LoadScene("Level01");
+
+        else
+        {
+            menuCanvas.SetActive(false);
+            levelCanvas.SetActive(true);
+        }
+
+        PlayerPrefs.SetInt("UnlockedLevel", 1);
+    }
+
+    private void PlayLevel(int index) => SceneManager.LoadScene("Level0" + index);
+    #endregion
+}
