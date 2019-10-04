74ed782209e1dfbec9e10e6e6747281e2ec30bee
1nsomnes <insomnes@protonmail.com>
2019-10-04 12:01:49 -0700

    [Big Push] Enlighten 2, created and added
    - PLEASE, ONLY USE 2019.2.4f1 for now on Enlightened2.
    - Do whatever you want with Enlighten
    - Please keep Enlighten 2 organized
    - Do not add unnecessary packages
    Changes:
    - Added touch controls
    - Fixed loads of bugs
    - Updated Spike and Player Colliders
    and more...

---
+++ Enlighten/Enlighten2/Assets/Scripts/Floater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    public float floatSpeed;
+    float originalY;
+    public float floatHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offsetY = (float)Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+        transform.position = new Vector2(transform.position.x, originalY + offsetY);
+    }
+}
